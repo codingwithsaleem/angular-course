fc0e1e7f3935c6d5639441c1a13a05df99d7db81
fitsaleem <saleem.raza@qavitechnologies.com>
2024-05-02 11:22:35 +0500

    angular modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import { InlineStyleComponentComponent } from './inline-style-component/inline-style-component.component';
 import { InlineTempleteComponentComponent } from './inline-templete-component/inline-templete-component.component';
 import { InlineComponentComponent } from './inline-component/inline-component.component';
@@ -12,8 +10,6 @@ import { UserAuthModule } from './user-auth/user-auth.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
     InlineStyleComponentComponent,
     InlineTempleteComponentComponent,
     InlineComponentComponent
